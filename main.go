97f5c8b514e28a8759febe58e4fc3d7ee094f4a8
Ed Andersen <mail@edandersen.com>
2022-04-20 16:04:31 +0100

    Handle app_offline.htm

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"gopkg.in/fsnotify.v1"
 )
 
 func main() {
@@ -23,6 +26,34 @@ func main() {
 			"foo": "bar",
 		})
 	})
+
+	// creates a new file watcher for App_offline.htm
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+	defer watcher.Close()
+
+	// watch for App_offline.htm and exit the program if present
+	// This allows continuous deployment on App Service as the .exe will not be
+	// terminated otherwise
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if strings.HasSuffix(event.Name, "App_offline.htm") {
+					fmt.Println("Exiting due to App_offline.htm being present")
+					os.Exit(0)
+				}
+			}
+		}
+	}()
+
+	// get the current working directory and watch it
+	currentDir, err := os.Getwd()
+	if err := watcher.Add(currentDir); err != nil {
+		fmt.Println("ERROR", err)
+	}
 
 	// Azure App Service sets the port as an Environment Variable
 	// This can be random, so needs to be loaded at startup
